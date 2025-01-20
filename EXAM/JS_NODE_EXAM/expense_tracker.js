3554d0471e2b6b494bd3554f6d1d8c4893fbe906
anamikaraj5 <anamikarajk561@gmail.com>
2025-01-20 13:35:19 +0530

    update

--- EXAM/JS_NODE_EXAM/expense_tracker.js
+++ EXAM/JS_NODE_EXAM/expense_tracker.js
@@ -8,44 +8,47 @@ const rl=readline.createInterface({
 function show()
 {
     console.log(`
-        1.Add Expense
+        1.Add Details
         2.Show Expense
         3.Exit
         `)
-    rl.question("Enter any option",Options)
+    rl.question("Enter any option ",Options)
 }
 
 categories=[]
 amount=[]
 date=[]
+// details=[]
+let total=0
 
 function Options(option)
 {
     switch(option)
     {
         case '1' :
-                 rl.question("Enter categories ",(input)=>
+                 rl.question("Enter categories : ",(input1)=>
                 {
-                    if(input!='')
+                    if(input1!='')
                     {
-                        categories.push(input)
-                        console.log("Category Added : ",input)
+                        categories.push(input1)
+                        console.log("Category Added : ",input1)
                     }
 
-                    rl.question("Enter expense ",(input)=>
+                    rl.question("Enter expense : ",(input2)=>
                     {
-                        if(input!='')
+                        if(input2!='')
                         {
-                            amount.push(parseInt(input))
-                            console.log("Expense Added : ",input)
+                            amount.push(input2)
+                            console.log("Expense Added : ",input2)
                         }
 
-                        rl.question("Enter date ",(input)=>
+                        rl.question("Enter date : ",(input3)=>
                             {
-                                if(input!='')
+                                if(input3!='')
                                 {
-                                    date.push(input)
-                                    console.log("Date Added : ",input)
+                                    date.push(input3)
+                                    // details.push({categories:input1 , amount:input2, date:input3})
+                                    console.log("Month Added : ",input3)
                                 }
                                 show()
                             })
@@ -56,14 +59,40 @@ function Options(option)
                 break
 
         case '2':
-                if(amount.length)
-                {
-                    if(date.length)
-                    {
-                        rl.question
-                    }
-                }
+                // console.log(details)
+                // rl.question("Enter the month ",Month)
+                // function Month(month){
+                //     switch(month)
+                //     {
+                //         case '1':
 
+                //     }
+                // }
+
+                categories1=categories.length?categories.join(','):"no categories given"
+                amount1=amount.length?amount.join(','):"no amount given"
+                date1=date.length?date.join(','):"no date given"
+                console.log(`Categories : ${categories1}`)
+                console.log(`Date : ${date1}`)
+                console.log(`Amount : ${amount1}`)
+                for(i of amount)
+                {
+                    total+=parseInt(i)
+                }
+                console.log(`Total Expense : ${total}`)
+                show()
+                break
+
+        case '3':
+                console.log("Exiting.....")
+                rl.close()
+                break
+
+
+        default:
+                console.log("Enter a valid option")
+                show()
+                break
     }
 }
 
